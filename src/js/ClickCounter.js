7f3140c6a10e1ce6680d8c102cff9c905a2b8c17
Stuart <stumax7@gmail.com>
2020-03-06 13:04:43 -0500

    Wiring buttons

--- src/js/ClickCounter.js
+++ src/js/ClickCounter.js
@@ -3,23 +3,22 @@ class ClickCount {
         this.clickCount = 0;
         this.companionCost = 100;
         this.companionCount = 0;
-        this.clickValue = 1;
-        console.log("In constructor")
         this.autoClick = () => {
             this.clickCount += this.companionCount;
-            // document.getElementById("displayCount").innerHTML = this.clickCount;
-            console.log(this.clickCount)
         };
     }
     countClick() {
         this.clickCount++;
-        // document.getElementById("displayCount").innerHTML = this.clickCount;
+        
     }
     getClickCount(){
         return this.clickCount;
     }
     getCompanionCount(){
         return this.companionCount;
+    }
+    getCompanionCost(){
+        return this.companionCost;
     }
     buyNewCompanion() {
         if (this.clickCount >= this.companionCost) {
@@ -28,9 +27,6 @@ class ClickCount {
             this.companionCost = Math.round(this.companionCost);
             this.companionCount++;
             let timer = setInterval(this.autoClick, 1000);
-            // document.getElementById("displayCompanionCost").innerHTML = this.companionCost;
-            // document.getElementById("displayCount").innerHTML = user.clickCount;
-            // document.getElementById("displayCompanionCount").innerHTML = this.companionCount;
         }
     }   
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,15 @@
-let clickCount = new ClickCount;
-clickCount.buyNewCompanion();
+let appCC = new ClickCount;
+
+const displayClickCount = document.querySelector(".displayCount").innerText = appCC.getClickCount();
+document.querySelector(".displayCompanionCost").innerText = appCC.getCompanionCost();
+document.querySelector(".displayCompanionCount").innerText = appCC.getCompanionCount();
+
+const makeButtonAddToClickCount = (displayClickCount, appCC) =>{
+    displayClickCount.addEventListener('click', ()=>{
+        appCC.countClick();
+    });
+}
+
+
+const clickButton = document.querySelector(".click-button");
+makeButtonAddToClickCount(clickButton, displayClickCount, appCC);
